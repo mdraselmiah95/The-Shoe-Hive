06a63f01a145219f359fbfaaceff56c03b738522
mdraselmiah95 <russelroy15@gmail.com>
2023-04-11 04:56:25 +0600

    WIP on THe SHoe Hive Hero Bannar

--- frontend/components/HeroBanner.jsx
+++ frontend/components/HeroBanner.jsx
@@ -6,7 +6,29 @@ import { Carousel } from "react-responsive-carousel";
 const HeroBanner = () => {
   return (
     <div className="relative text-white text-[20px] w-full max-w-[1360px] mx-auto">
-      <Carousel>
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showIndicators={false}
+        showStatus={false}
+        renderArrowPrev={(clickHandler, hasPrev) => (
+          <div
+            onClick={clickHandler}
+            className="absolute right-[31px] md:right-[51px] bottom-0 w-[30px] md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center justify-center cursor-pointer hover:opacity-90"
+          >
+            <BiArrowBack className="text-sm md:text-lg" />
+          </div>
+        )}
+        renderArrowNext={(clickHandler, hasNext) => (
+          <div
+            onClick={clickHandler}
+            className="absolute right-0 bottom-0 w-[30px] md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center justify-center cursor-pointer hover:opacity-90"
+          >
+            <BiArrowBack className="text-sm rotate-180 md:text-lg" />
+          </div>
+        )}
+      >
         <div>
           <img
             src="/slide-1.png"
