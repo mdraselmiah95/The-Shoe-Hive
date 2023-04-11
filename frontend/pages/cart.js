06fc43a63497582ceec8d61cd263584f8c479d98
mdraselmiah95 <russelroy15@gmail.com>
2023-04-12 05:54:04 +0600

    WIP on The Shoe Hive Cart

--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -1,4 +1,6 @@
 import Wrapper from "@/components/Wrapper";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const cart = () => {
@@ -53,6 +55,28 @@ const cart = () => {
           </div>
           {/* CART CONTENT END */}
         </>
+
+        {/* This is empty screen */}
+        <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
+          <Image
+            src="/empty-cart.jpg"
+            width={300}
+            height={300}
+            className="w-[300px] md:w-[400px]"
+          />
+          <span className="text-xl font-bold">Your cart is empty</span>
+          <span className="mt-4 text-center">
+            Looks like you have not added anything in your cart.
+            <br />
+            Go ahead and explore top categories.
+          </span>
+          <Link
+            href="/"
+            className="px-8 py-4 mt-8 mb-3 text-lg font-medium text-white transition-transform bg-black rounded-full active:scale-95 hover:opacity-75"
+          >
+            Continue Shopping
+          </Link>
+        </div>
       </Wrapper>
     </div>
   );
