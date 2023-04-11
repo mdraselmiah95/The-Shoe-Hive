d85e5b8cf3d61434a2f6adf802b85b7dd224e38b
mdraselmiah95 <russelroy15@gmail.com>
2023-04-12 03:21:30 +0600

    WIP on The Shoe Hive description

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,28 @@
 import HeroBanner from "@/components/HeroBanner";
+import Wrapper from "@/components/Wrapper";
 
 export default function Home() {
   return (
     <>
       <main>
         <HeroBanner />
+        <Wrapper>
+          {/* heading and paragraph start */}
+          <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+            <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+              Cushioning for Your Miles
+            </div>
+            <div className="text-md md:text-xl">
+              Experience ultimate comfort and support with Nike's ZoomX and
+              elevated stack heights, designed to cushion your stride during
+              long runs. Unleash your potential with every step.
+            </div>
+          </div>
+          {/* heading and paragraph end */}
+        </Wrapper>
       </main>
     </>
   );
 }
 
-// video=> 00:33:00
+// video=> 01:51:00
