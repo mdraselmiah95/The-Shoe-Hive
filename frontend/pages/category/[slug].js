376b21e9c75f36f1b80f063d957f454849171338
mdraselmiah95 <russelroy15@gmail.com>
2023-04-12 04:50:00 +0600

    WIP on The Shoe Hive Category

--- frontend/pages/category/[slug].js
+++ frontend/pages/category/[slug].js
@@ -1,12 +1,33 @@
-import { useRouter } from "next/router";
+import ProductCard from "@/components/ProductCard";
+import Wrapper from "@/components/Wrapper";
 import React from "react";
 
 const Category = () => {
-  const router = useRouter();
-  const { slug } = router.query;
-  console.log(slug);
+  return (
+    <div className="relative w-full md:py-20">
+      <Wrapper>
+        <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+            Running Shoes
+          </div>
+        </div>
 
-  return <div>Category</div>;
+        {/* products grid start */}
+        <div className="grid grid-cols-1 gap-5 px-5 md:grid-cols-2 lg:grid-cols-3 my-14 md:px-0">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+        {/* products grid end */}
+      </Wrapper>
+    </div>
+  );
 };
 
 export default Category;
