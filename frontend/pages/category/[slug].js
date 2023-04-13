d133788d06898ed545456f3002623cedafc40ba1
mdraselmiah95 <russelroy15@gmail.com>
2023-04-13 07:11:47 +0600

    WIP on The Shoe Hive show Products category and products

--- frontend/pages/category/[slug].js
+++ frontend/pages/category/[slug].js
@@ -1,30 +1,29 @@
+import Loader from "@/components/Loader";
 import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
 import { fetchDataFromApi } from "@/utils/api";
 import React from "react";
 
-const Category = () => {
+const Category = ({ category, products, slug }) => {
   return (
     <div className="relative w-full md:py-20">
       <Wrapper>
         <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
           <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-            Running Shoes
+            {category?.data?.[0]?.attributes?.name}
           </div>
         </div>
 
         {/* products grid start */}
-        <div className="grid grid-cols-1 gap-5 px-5 md:grid-cols-2 lg:grid-cols-3 my-14 md:px-0">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-        </div>
+        {!products.data ? (
+          <Loader />
+        ) : (
+          <div className="grid grid-cols-1 gap-5 px-5 md:grid-cols-2 lg:grid-cols-3 my-14 md:px-0">
+            {products?.data?.map((product) => (
+              <ProductCard key={product?.id} data={product} />
+            ))}
+          </div>
+        )}
         {/* products grid end */}
       </Wrapper>
     </div>
@@ -35,7 +34,7 @@ export default Category;
 
 export async function getStaticPaths() {
   const category = await fetchDataFromApi("/api/categories?populate=*");
-  const paths = category.data.map((c) => ({
+  const paths = category?.data?.map((c) => ({
     params: {
       slug: c.attributes.slug,
     },
@@ -46,14 +45,19 @@ export async function getStaticPaths() {
     fallback: false,
   };
 }
-
 export async function getStaticProps({ params: { slug } }) {
   const category = await fetchDataFromApi(
     `/api/categories?filters[slug][$eq]=${slug}`
   );
+  const products = await fetchDataFromApi(
+    `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}`
+  );
+
   return {
     props: {
       category,
+      products,
+      slug,
     },
   };
 }
